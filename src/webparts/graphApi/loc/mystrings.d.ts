c053b2b0b70892e3858ecc4c4ae5d270d1429b3e
jaydeep08 <jaydeeppatidar008@gmail.com>
2024-07-28 15:15:52 +0530

    first commit

---
+++ src/webparts/graphApi/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IGraphApiWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'GraphApiWebPartStrings' {
+  const strings: IGraphApiWebPartStrings;
+  export = strings;
+}
